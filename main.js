d6d2abdeb5e343a4397260fe4ebf4e1a6061caab
Ben Pastel <benpastel@gmail.com>
2017-08-01 00:22:18 -0400

    functional restart button\!

--- main.js
+++ main.js
@@ -3,25 +3,28 @@ const ORIGIN_X = 10;
 const ORIGIN_Y = 10;
 const MAX_BOARD_X = ORIGIN_X + SQUARE_SIDE * N_COLS;
 const MAX_BOARD_Y = ORIGIN_Y + SQUARE_SIDE * N_ROWS;
-const paper = Raphael(0, 0, 1000, 1000);
+const paper = Raphael("board", 580, 560); 
+console.log("Y: " + MAX_BOARD_Y)
+console.log("X: " + MAX_BOARD_X)
+// TODO: relative units to rescale gracefully on smaller browser window
 
 const messageX = toPixels(-1, 3).x;
 const messageY = toPixels(-1, 3).y;
-
 const ANIMATION_MILLIS = 500;
 
-// initialize display
-for (var row = 0; row < N_ROWS+1; row++) {
-	var y = ORIGIN_Y + row * SQUARE_SIDE;
-	paper.path(
-		"M" + ORIGIN_X + "," + y +
-		"L" + (ORIGIN_X + SQUARE_SIDE * N_COLS) + "," + y);
-}
-for (var col = 0; col < N_COLS+1; col++) {
-	var x = ORIGIN_X + col * SQUARE_SIDE;
-	paper.path(
-		"M" + x + "," + ORIGIN_Y + 
-		"L" + x + "," + (ORIGIN_Y + SQUARE_SIDE * N_ROWS));
+function initDisplay() {
+	for (var row = 0; row < N_ROWS+1; row++) {
+		var y = ORIGIN_Y + row * SQUARE_SIDE;
+		paper.path(
+			"M" + ORIGIN_X + "," + y +
+			"L" + (ORIGIN_X + SQUARE_SIDE * N_COLS) + "," + y);
+	}
+	for (var col = 0; col < N_COLS+1; col++) {
+		var x = ORIGIN_X + col * SQUARE_SIDE;
+		paper.path(
+			"M" + x + "," + ORIGIN_Y + 
+			"L" + x + "," + (ORIGIN_Y + SQUARE_SIDE * N_ROWS));
+	}
 }
 
 // pixels of the center of the square
@@ -30,7 +33,6 @@ function toPixels(row, col) {
 		x: ORIGIN_X + (col + 0.5) * SQUARE_SIDE,
 		y: ORIGIN_Y + (N_ROWS - row - 1 + 0.5) * SQUARE_SIDE
 	}
-
 }
 
 // TODO: optional ATTRs into message
@@ -56,9 +58,8 @@ function onClick(e) {
     var x = e.pageX;
     var y = e.pageY;
 
-    // allow y < ORIGIN_Y because that's like dropping a piece
     if (x > MAX_BOARD_X || y > MAX_BOARD_Y || x < ORIGIN_X) {
-    	message("you missed the board");
+    	// off the board; ignore
 		return;
     }
 
@@ -119,8 +120,6 @@ function end(result_check) {
 	}
 }
 
-document.addEventListener("click", onClick);
-message("click on a column to drop a piece");
 function main_loop(row, col) {
 	message("");
 	global_board[row + col * N_ROWS] = global_player;
@@ -144,11 +143,18 @@ function main_loop(row, col) {
 	}, ANIMATION_MILLIS + 100);
 }
 
+// TODO: wipe the pieces from the board
 function restart() {
 	console.log("restarting");
 	reset_shared_state();
 	reset_bot_state();
+	paper.clear();
+	initDisplay();
 	message("new game");
 }
+
+initDisplay();
+document.addEventListener("click", onClick);
+message("click on a column to drop a piece");
 
 
